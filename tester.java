fee0e2fad616db7296b58bb28b4098853b109799
Douglas Kaelin <11dkaelin@gmail.com>
2017-12-02 23:59:28 -0500

    creating files

---
+++ tester.java
@@ -0,0 +1,17 @@
+
+
+
+public class tester{
+   public static void main(String[] args){
+      new tester();
+   }
+   
+   public tester(){
+      /*
+      make new keys
+      encrypt some text
+      decrypt the text
+      */
+   }
+
+}
